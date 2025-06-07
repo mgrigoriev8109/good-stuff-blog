4b64ab9b83c4b10dbbe043ddb3692d8e57ce596c
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2025-06-07 14:56:53 -0400

    feat: add in creation of new articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,4 +10,23 @@ class ArticlesController < ApplicationController
     # access to whichever id we pass in via the url
     @article = Article.find(params[:id])
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
