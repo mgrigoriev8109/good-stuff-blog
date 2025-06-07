c1a7912663b9fd989d9319e21d3b42a4db0975db
Mikhail Grigoriev <mikhailgrigoriev92@gmail.com>
2025-06-07 08:07:34 -0400

    feat: add viewing of a blog post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    # the :id can be accessed in this show function
+    # via params[], due to the fact that inside the routes.rb
+    # file we stated get "/articles/:id", which gives the function
+    # access to whichever id we pass in via the url
+    @article = Article.find(params[:id])
+  end
 end
